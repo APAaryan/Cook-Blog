6c19c9c826b850ddc32e791b827128880068d3db
Aaryan <116apaaryan@gmail.com>
2022-10-30 13:31:09 +0530

    v1.0.3

--- controller/imagecontroller.js
+++ controller/imagecontroller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url= 'https://apcookblog.herokuapp.com/' || 'http://localhost:8000';
+const url= 'https://apcookblog.herokuapp.com' || 'http://localhost:8000';
 
 let gfs,gridfsBucket;
 
